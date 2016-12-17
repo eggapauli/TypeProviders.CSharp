1e092a0d3d6c36bc195b08a5d9cc81e14888d031
Johannes Egger <eggerjohansi@gmail.com>
2016-12-17 23:29:09 +0100

    Extract CodeRefactoringProviderHelper

---
+++ TypeProviders.CSharp.CodeRefactoringProvider/CodeRefactoringProviderHelper.fs
@@ -0,0 +1,93 @@
+﻿module TypeProviders.CSharp.CodeRefactoringProvider.CodeRefactoringProviderHelper
+
+open System
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.CSharp
+open Microsoft.CodeAnalysis.CSharp.Syntax
+open Microsoft.CodeAnalysis.Formatting
+open Microsoft.CodeAnalysis.CodeActions
+open Microsoft.CodeAnalysis.CodeRefactorings
+open TypeProviders.CSharp
+
+let tryGetTypeProviderSampleData (typeDecl: SyntaxNode) typeProviderAttributeName (semanticModel: SemanticModel) =
+    semanticModel
+        .Compilation
+        .GetTypeByMetadataName typeProviderAttributeName
+    |> Option.ofObj
+    |> Option.bind (fun attributeSymbol ->
+        let typeSymbol = semanticModel.GetDeclaredSymbol typeDecl
+            
+        typeSymbol.GetAttributes()
+        |> Seq.tryFind(fun attr -> attr.AttributeClass.Equals attributeSymbol)
+    )
+    |> Option.bind(fun attribute -> attribute.ConstructorArguments |> Seq.tryHead)
+    |> Option.bind(fun sampleSourceArgument ->
+        sampleSourceArgument.Value
+        |> Option.cast<string>
+    )
+
+let updateTypeProviderAsync (document: Document) (typeDecl: ClassDeclarationSyntax) sampleData generateMembers ct =
+    async {
+        let! syntaxRoot = document.GetSyntaxRootAsync ct |> Async.awaitTaskAllowContextSwitch
+
+        try
+            let members = generateMembers sampleData
+
+            let newTypeDecl =
+                typeDecl
+                    .WithMembers(SyntaxFactory.List members)
+                    .WithAdditionalAnnotations(Formatter.Annotation)
+            return
+                syntaxRoot.ReplaceNode(typeDecl, newTypeDecl)
+                |> document.WithSyntaxRoot
+        with e ->
+            let newTypeDecl =
+                typeDecl
+                    .WithMembers(SyntaxFactory.List<MemberDeclarationSyntax>())
+                    .WithOpenBraceToken(
+                        SyntaxFactory
+                            .Token(SyntaxKind.OpenBraceToken)
+                            .WithTrailingTrivia(
+                                SyntaxFactory.CarriageReturnLineFeed,
+                                SyntaxFactory.Comment <| sprintf "/* %s */" e.Message,
+                                SyntaxFactory.CarriageReturnLineFeed
+                            )
+                        );
+            return
+                syntaxRoot.ReplaceNode(typeDecl, newTypeDecl)
+                |> document.WithSyntaxRoot
+    }
+
+let getSemanticModel (document: Document) ct =
+    document.GetSemanticModelAsync ct
+    |> Async.awaitTaskAllowContextSwitch
+
+let createCodeAction document typeDecl generateMembers sampleData =
+    CodeAction.Create(
+        "Generate members from sample data",
+        Func<_, _>(updateTypeProviderAsync document typeDecl sampleData generateMembers >> Async.StartAsTask)
+    )
+
+let findContextNode (context: CodeRefactoringContext) = async {
+    let! root =
+        context
+            .Document
+            .GetSyntaxRootAsync(context.CancellationToken)
+        |> Async.awaitTaskAllowContextSwitch
+
+    return root.FindNode context.Span
+}
+
+let getRefactorings (context: CodeRefactoringContext) attributeFullName generateMembers ct = async {
+    let document = context.Document
+    let! node = findContextNode context
+    match node with
+    | :? ClassDeclarationSyntax as typeDecl ->
+        let! semanticModel = getSemanticModel document ct
+
+        return
+            tryGetTypeProviderSampleData typeDecl attributeFullName semanticModel
+            |> Option.map (createCodeAction document typeDecl generateMembers)
+            |> Option.toList
+    | _ ->  return []
+}
--- TypeProviders.CSharp.CodeRefactoringProvider/JsonProviderCodeRefactoringProvider.fs
+++ TypeProviders.CSharp.CodeRefactoringProvider/JsonProviderCodeRefactoringProvider.fs
@@ -1,121 +1,31 @@
-namespace TypeProviders.CSharp
+namespace TypeProviders.CSharp.CodeRefactoringProvider
 
-open System
 open System.Composition
-open System.Threading
-open Microsoft.CodeAnalysis
-open Microsoft.CodeAnalysis.CodeActions
 open Microsoft.CodeAnalysis.CodeRefactorings
-open Microsoft.CodeAnalysis.CSharp
-open Microsoft.CodeAnalysis.CSharp.Syntax
-open Microsoft.CodeAnalysis.Formatting
-open ProviderImplementation
+open Microsoft.CodeAnalysis
+open TypeProviders.CSharp
 
 [<ExportCodeRefactoringProvider(LanguageNames.CSharp, Name = "JsonProviderCodeRefactoringProvider")>]
 [<Shared>]
 type JsonProviderCodeRefactoringProvider() =
     inherit CodeRefactoringProvider()
 
-    let attributeFullName = typeof<JsonProviderAttribute>.FullName
-
-    let tryGetTypeProviderSampleData (typeDecl: SyntaxNode) typeProviderAttributeName (semanticModel: SemanticModel) =
-        semanticModel
-            .Compilation
-            .GetTypeByMetadataName typeProviderAttributeName
-        |> Option.ofObj
-        |> Option.bind (fun attributeSymbol ->
-            let typeSymbol = semanticModel.GetDeclaredSymbol typeDecl
-            
-            typeSymbol.GetAttributes()
-            |> Seq.tryFind(fun attr -> attr.AttributeClass.Equals attributeSymbol)
-        )
-        |> Option.bind(fun attribute -> attribute.ConstructorArguments |> Seq.tryHead)
-        |> Option.bind(fun sampleSourceArgument ->
-            sampleSourceArgument.Value
-            |> Option.cast<string>
-        )
-
-    let updateTypeProviderAsync (document: Document) (typeDecl: ClassDeclarationSyntax) sampleData parseSampleData ct =
+    override x.ComputeRefactoringsAsync (context: CodeRefactoringContext) =
         async {
-            let! syntaxRoot = document.GetSyntaxRootAsync ct |> Async.awaitTaskAllowContextSwitch
-
-            try
+            let generateMembers sampleData =
                 let dataType = 
-                    parseSampleData sampleData
+                    JsonProviderArgs.create sampleData
+                    |> JsonProviderBridge.parseDataType
                     |> DataTypeUpdate.CSharp.ensureTypeHasNoPropertyWithSameName
 
-                let members = [
+                [
                     yield! JsonCodeGeneration.generateDataStructure dataType
                     yield! JsonCodeGeneration.generateCreationMethods dataType sampleData
                 ]
 
-                let newTypeDecl =
-                    typeDecl
-                        .WithMembers(SyntaxFactory.List members)
-                        .WithAdditionalAnnotations(Formatter.Annotation)
-                return
-                    syntaxRoot.ReplaceNode(typeDecl, newTypeDecl)
-                    |> document.WithSyntaxRoot
-            with e ->
-                let newTypeDecl =
-                    typeDecl
-                        .WithMembers(SyntaxFactory.List<MemberDeclarationSyntax>())
-                        .WithOpenBraceToken(
-                            SyntaxFactory
-                                .Token(SyntaxKind.OpenBraceToken)
-                                .WithTrailingTrivia(
-                                    SyntaxFactory.CarriageReturnLineFeed,
-                                    SyntaxFactory.Comment <| sprintf "/* %s */" e.Message,
-                                    SyntaxFactory.CarriageReturnLineFeed
-                                )
-                            );
-                return
-                    syntaxRoot.ReplaceNode(typeDecl, newTypeDecl)
-                    |> document.WithSyntaxRoot
-        }
+            let attributeFullName = typeof<JsonProviderAttribute>.FullName
 
-    let getSemanticModel (document: Document) ct =
-        document.GetSemanticModelAsync ct
-        |> Async.awaitTaskAllowContextSwitch
-
-    let createCodeAction document typeDecl parseSampleData sampleData =
-        CodeAction.Create(
-            "Generate members from sample data",
-            Func<_, _>(updateTypeProviderAsync document typeDecl sampleData parseSampleData >> Async.StartAsTask)
-        )
-
-    let getRefactorings document typeDecl attributeFullName parseSampleData ct = async {
-        let! semanticModel = getSemanticModel document ct
-
-        return
-            tryGetTypeProviderSampleData typeDecl attributeFullName semanticModel
-            |> Option.map (createCodeAction document typeDecl parseSampleData)
-            |> Option.toList
-    }
-
-    let findContextNode (context: CodeRefactoringContext) = async {
-        let! root =
-            context
-                .Document
-                .GetSyntaxRootAsync(context.CancellationToken)
-            |> Async.awaitTaskAllowContextSwitch
-
-        return root.FindNode context.Span
-    }
-
-    override x.ComputeRefactoringsAsync (context: CodeRefactoringContext) =
-        async {
-            let! node = findContextNode context
-
-            let parseSampleData =
-                JsonProviderArgs.create
-                >> JsonProviderBridge.parseDataType
-
-            let! refactorings =
-                match node with
-                | :? ClassDeclarationSyntax as typeDecl ->
-                    getRefactorings context.Document typeDecl attributeFullName parseSampleData context.CancellationToken
-                | _ ->  async { return [] }
+            let! refactorings = CodeRefactoringProviderHelper.getRefactorings context attributeFullName generateMembers context.CancellationToken
 
             refactorings
             |> List.iter context.RegisterRefactoring

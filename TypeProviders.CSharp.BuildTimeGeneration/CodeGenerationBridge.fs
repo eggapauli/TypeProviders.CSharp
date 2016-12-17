c73ed0cd245f7fe5461e71145afd2d011fee8e3e
Johannes Egger <eggerjohansi@gmail.com>
2016-12-17 23:26:48 +0100

    Extract CodeGenerationBridge

---
+++ TypeProviders.CSharp.BuildTimeGeneration/CodeGenerationBridge.fs
@@ -0,0 +1,69 @@
+﻿namespace TypeProviders.CSharp.BuildTimeGeneration
+
+open System
+open System.Threading.Tasks
+open Microsoft.CodeAnalysis
+open Microsoft.CodeAnalysis.CSharp
+open Microsoft.CodeAnalysis.CSharp.Syntax
+open TypeProviders.CSharp
+
+type DiagnosticsType =
+    | Debug of string
+    | GeneralError of string * string
+
+module Diagnostics =
+    let toDescriptor = function
+        | Debug message ->
+            DiagnosticDescriptor(
+                "TPCS001",
+                "Debug message",
+                message,
+                "",
+                DiagnosticSeverity.Error,
+                isEnabledByDefault = true,
+                description = null,
+                helpLinkUri = null
+            )
+        | GeneralError (title, message) ->
+            DiagnosticDescriptor(
+                "TPCS002",
+                title,
+                message,
+                "",
+                DiagnosticSeverity.Error,
+                isEnabledByDefault = true,
+                description = null,
+                helpLinkUri = null
+            )
+    let create location diagnosticsType = 
+        Diagnostic.Create(toDescriptor diagnosticsType, location)
+
+module CodeGenerationBridge =
+    let generate (applyTo: MemberDeclarationSyntax) (progress: IProgress<Diagnostic>) fn =
+        applyTo
+        :?> ClassDeclarationSyntax
+        |> Option.ofObj
+        |> Option.map (fun typeDecl ->
+            let members =
+                try
+                    fn()
+                with e ->
+                    GeneralError ("Error while generating code", e.Message)
+                    |> Diagnostics.create (applyTo.GetLocation())
+                    |> progress.Report
+
+                    reraise(); []
+
+            let partialClass =
+                SyntaxFactory
+                    .ClassDeclaration(typeDecl.Identifier)
+                    .AddModifiers(SyntaxFactory.Token SyntaxKind.PartialKeyword)
+                    .WithMembers(SyntaxFactory.List members)
+
+            partialClass
+            :> MemberDeclarationSyntax
+            |> List.singleton
+        )
+        |> Option.ifNone []
+        |> SyntaxFactory.List
+        |> Task.FromResult
--- TypeProviders.CSharp.BuildTimeGeneration/JsonProviderGenerator.fs
+++ TypeProviders.CSharp.BuildTimeGeneration/JsonProviderGenerator.fs
@@ -1,97 +1,25 @@
 ﻿namespace TypeProviders.CSharp.BuildTimeGeneration
 
 open System
-open System.Threading
-open System.Threading.Tasks
-open CodeGeneration.Roslyn
 open Microsoft.CodeAnalysis
-open Microsoft.CodeAnalysis.CSharp
 open Microsoft.CodeAnalysis.CSharp.Syntax
+open CodeGeneration.Roslyn
 open TypeProviders.CSharp
-
-type DiagnosticsType =
-    | Debug of string
-    | GeneralError of string * string
-
-[<AutoOpen>]
-module Diagnostics_ =
-    module Diagnostics =
-        let toDescriptor = function
-            | Debug message ->
-                DiagnosticDescriptor(
-                    "TPCS001",
-                    "Debug message",
-                    message,
-                    "",
-                    DiagnosticSeverity.Error,
-                    isEnabledByDefault = true,
-                    description = null,
-                    helpLinkUri = null
-                )
-            | GeneralError (title, message) ->
-                DiagnosticDescriptor(
-                    "TPCS002",
-                    title,
-                    message,
-                    "",
-                    DiagnosticSeverity.Error,
-                    isEnabledByDefault = true,
-                    description = null,
-                    helpLinkUri = null
-                )
-        let create location diagnosticsType = 
-            Diagnostic.Create(toDescriptor diagnosticsType, location)
 
 type JsonProviderGenerator(attributeData: AttributeData) =
     let sampleData = attributeData.ConstructorArguments.[0].Value :?> string
 
     interface ICodeGenerator with
         member x.GenerateAsync(applyTo: MemberDeclarationSyntax, document: Document, progress: IProgress<Diagnostic>, ct) =
-//             [
-//                 yield sprintf "Location: %s" (System.Reflection.Assembly.GetExecutingAssembly().Location)
-//             ]
-//             |> String.concat Environment.NewLine
-//             |> Debug
-//             |> Diagnostics.create (applyTo.GetLocation())
-//             |> progress.Report
-//             Task.FromResult (SyntaxFactory.List())
-           let parseSampleData =
-               JsonProviderArgs.create
-               >> JsonProviderBridge.parseDataType
-
-           applyTo
-           :?> ClassDeclarationSyntax
-           |> Option.ofObj
-           |> Option.map (fun typeDecl ->
-               let members =
-                   try
-                       let dataType = 
-                           parseSampleData sampleData
-                           |> DataTypeUpdate.CSharp.ensureTypeHasNoPropertyWithSameName
+            let generateMembers() =
+                let dataType = 
+                    JsonProviderArgs.create sampleData
+                    |> JsonProviderBridge.parseDataType
+                    |> DataTypeUpdate.CSharp.ensureTypeHasNoPropertyWithSameName
                    
-                       [
-                           yield! JsonCodeGeneration.generateDataStructure dataType
-                           yield! JsonCodeGeneration.generateCreationMethods dataType sampleData
-                       ]
-                   with e ->
-                       GeneralError ("Error while generating code", e.Message)
-                       |> Diagnostics.create (applyTo.GetLocation())
-                       |> progress.Report
+                [
+                    yield! JsonCodeGeneration.generateDataStructure dataType
+                    yield! JsonCodeGeneration.generateCreationMethods dataType sampleData
+                ]
 
-                       reraise()
-
-                       []
-
-               let partialClass =
-                   SyntaxFactory
-                       .ClassDeclaration(typeDecl.Identifier)
-                       .AddModifiers(SyntaxFactory.Token SyntaxKind.PartialKeyword)
-                       .WithMembers(SyntaxFactory.List members)
-
-               partialClass
-               :> MemberDeclarationSyntax
-               |> List.singleton
-           )
-           |> Option.ifNone []
-           |> SyntaxFactory.List
-           |> Task.FromResult
+            CodeGenerationBridge.generate applyTo progress generateMembers
